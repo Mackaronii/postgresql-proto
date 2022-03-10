1132d2188a9ee317a727732621ea20630319c152
Jonathan Mack <johnny.mack1998@gmail.com>
2022-03-10 18:45:25 -0500

    Updated database schema.

---
+++ dbConfig.sql
@@ -0,0 +1,50 @@
+CREATE TABLE survey
+(surveyID INTEGER NOT NULL,
+ userID INTEGER NOT NULL,
+ surveyName TEXT NOT NULL,
+ isOpen BOOLEAN NOT NULL,
+ PRIMARY KEY (surveyID));
+
+
+CREATE TABLE question
+(questionID INTEGER NOT NULL,
+ surveyID INTEGER NOT NULL,
+ questionOrder character varying NOT NULL,
+ questionType INTEGER NOT NULL,
+ questionPrompt INTEGER NOT NULL,
+ PRIMARY KEY (questionID));
+
+
+CREATE TABLE users
+(userID INTEGER NOT NULL,
+ username TEXT NOT NULL,
+ PRIMARY KEY (userID));
+
+
+CREATE TABLE answer
+(answerID INTEGER NOT NULL,
+ surveyID INTEGER NOT NULL,
+ questionID INTEGER NOT NULL,
+ answerText TEXT NOT NULL,
+ PRIMARY KEY (answerID));
+
+
+ALTER TABLE question
+  ADD CONSTRAINT FK_question_TO_survey
+    FOREIGN KEY (surveyID)
+    REFERENCES survey(surveyID);
+
+ALTER TABLE answer
+  ADD CONSTRAINT FK_answer_TO_survey
+    FOREIGN KEY (surveyID)
+    REFERENCES survey(surveyID);
+
+ALTER TABLE survey
+  ADD CONSTRAINT FK_survey_TO_users
+    FOREIGN KEY (userID)
+    REFERENCES users(userID);
+
+ALTER TABLE answer
+  ADD CONSTRAINT FK_answer_TO_question
+    FOREIGN KEY (questionID)
+    REFERENCES question(questionID);
